7d58e52dfb6530cff612b1c75d7366f1fb176d67
Ayaz Alavi <ayaz@pursue.today>
2021-11-15 02:39:10 +0500

    new index file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
-import 'dotenv/config';
-import App from '@/app';
-import AuthRoute from '@routes/auth.route';
-import IndexRoute from '@routes/index.route';
-import UsersRoute from '@/routes/profile.route';
-import validateEnv from '@utils/validateEnv';
+require('dotenv').config()
+ import AuthRoute from '@routes/auth.route';
+ import IndexRoute from '@routes/index.route';
+ import validateEnv from '@utils/validateEnv';
+import App from './app';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute()]);
 
 app.listen();
