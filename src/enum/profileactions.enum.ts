7bb5b4dcdbd6ab491b80f196ff4c92956ea1db96
Ayaz Alavi <ayaz@pursue.today>
2021-11-27 20:07:05 +0500

    new enums folder

---
+++ src/enum/profileactions.enum.ts
@@ -0,0 +1,17 @@
+export enum ProfileActions {
+    ACCOUNT_TYPE,
+    LOCATION,
+    BIRTHDAY,
+    BUSINESS_INTERESTS,
+    TAGS,
+    FRIEND,
+    SUBSCRIPTION,
+    Images,
+    BUSINESS_INFO,
+    BUSINESS_DETAILS,
+    ADD_AWARD,
+    ADD_TALENT,
+    PERSONAL_INFO,
+    ADD_EXPERIENCE,
+    EMAIL,
+}
