939e173000705ed8ab709fe9db4ac20dca37d337
Ayaz Alavi <ayaz@pursue.today>
2021-11-27 20:34:03 +0500

    commented env

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ require('dotenv').config()
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import ProfileRoute from '@routes/profile.route';
-import validateEnv from '@utils/validateEnv';
+//import validateEnv from '@utils/validateEnv';
 import App from './src//app';
 
-validateEnv();
+//validateEnv();
 
 const app = new App([new IndexRoute(), new AuthRoute(), new ProfileRoute()]);
 
