4e727ddbd5f78be65c2e7f407a39f5a36b6305a7
Ayaz Alavi <ayaz@pursue.today>
2021-11-27 14:39:07 +0500

    added profile routes in server.ts

--- server.ts
+++ server.ts
@@ -2,11 +2,12 @@ process.env['NODE_CONFIG_DIR'] = __dirname + '/src/configs';
 require('dotenv').config()
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
+import ProfileRoute from '@routes/profile.route';
 import validateEnv from '@utils/validateEnv';
 import App from './src//app';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new ProfileRoute()]);
 
 app.listen();
