0f6fb22e6fcf3c89cca9bd5a4e12fd2be32e75c7
Ayaz Alavi <ayaz@pursue.today>
2021-11-27 14:43:06 +0500

    added profile routes

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,25 +1,21 @@
 import { Router } from 'express';
-import UsersController from '@controllers/users.controller';
-import { SignUpDTO } from '@/dtos/signup.dto';
 import { Routes } from '@interfaces/routes.interface';
-import validationMiddleware from '@middlewares/validation.middleware';
+import ProfileController from '@/controllers/profile.controller';
+import authMiddleware from '@middlewares/auth.middleware';
 
-class UsersRoute implements Routes {
-  public path = '/users';
+class ProfileRoute implements Routes {
+  public path = '/profile';
   public router = Router();
-  public usersController = new UsersController();
+  public profileController = new ProfileController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.usersController.getUsers);
-    this.router.get(`${this.path}/:id`, this.usersController.getUserById);
-    this.router.post(`${this.path}`, validationMiddleware(SignUpDTO, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:id`, validationMiddleware(SignUpDTO, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id`, this.usersController.deleteUser);
+    this.router.get(`${this.path}/:id?`, authMiddleware, this.profileController.profile);
+    this.router.post(`${this.path}`, authMiddleware, this.profileController.updateProfile);    
   }
 }
 
-export default UsersRoute;
+export default ProfileRoute;
