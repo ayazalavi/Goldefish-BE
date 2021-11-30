0675f586aa763d3bf439ef5eaefcf5d52ee18719
Ayaz Alavi <ayaz@pursue.today>
2021-11-30 23:11:01 +0500

    Updated profile route to handle multiple on boarding requests

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -4,6 +4,7 @@ import ProfileController from '@/controllers/profile.controller';
 import authMiddleware from '@middlewares/auth.middleware';
 import { ProfileDTO } from '@/dtos/profile.dto';
 import validationMiddleware from '@/middlewares/validation.middleware';
+import { ActionDTO } from '@/dtos/action.dto';
 
 class ProfileRoute implements Routes {
   public path = '/profile';
@@ -16,7 +17,10 @@ class ProfileRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/:id?`, authMiddleware, this.profileController.profile);
-    this.router.post(`${this.path}`, authMiddleware, validationMiddleware(ProfileDTO, 'body'), this.profileController.updateProfile);    
+    this.router.post(`${this.path}/:action?`, authMiddleware,
+          validationMiddleware(ActionDTO, 'params'),
+          validationMiddleware(ProfileDTO, 'body'),
+          this.profileController.updateProfile);
   }
 }
 
