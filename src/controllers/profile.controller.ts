0496fa41adac79a95a199efbd2f0a4264179c800
Ayaz Alavi <ayaz@pursue.today>
2021-12-01 00:00:07 +0500

    profile controller updated for save requests

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -26,8 +26,8 @@ class ProfileController {
   public updateProfile = async (req: RequestWithUser, res: Response, next: NextFunction) => {
     try {   
       //const updateUserData: user = await this.profileService.updateUser(userId, userData);
-      console.log(req.body)
-      res.status(200).json({ data: [], message: 'updated' });
+      let newuser = await this.profileService.updateUser(req.user, req.body)
+      res.status(200).json({ data: newuser, message: 'updated' });
     } catch (error) {
       next(error);
     }
